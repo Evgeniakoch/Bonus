caef8cef4775ffc740b9308ccffdad8397e6f742
Jane <evgenia.kochukova@gmail.com>
2023-02-01 18:12:01 -0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,20 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 200;
-        int amount = 1200;
+        int amount = 1600;
+        int bonus = amount / 100;
 
         System.out.println("Баланс:");
 
         if (amount > 1000) {
-            System.out.println((amount / 100) + amount + balance);
+            System.out.println(bonus + amount + balance);
         } else {
             System.out.println(amount + balance);
         }
         System.out.println("Сумма бонусов:");
 
         if (amount > 1000) {
-            System.out.println(amount / 100);
+            System.out.println(bonus);
         } else {
             System.out.println("0");
         }
